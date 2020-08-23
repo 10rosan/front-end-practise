5a057569da388d802b0bf03c48199db56ba289b1
10rosan <roshan.adhikari.se@gmail.com>
2020-08-23 11:47:13 +0545

    front end small work

---
+++ practise.js
@@ -0,0 +1,15 @@
+//how to create a private variable in JavaScript?
+
+// function secretVariable(){
+//     var private = "super secret code";
+//     return function(){
+//         return private
+//     }
+// }
+
+// var getPrivateVariable = secretVariable()
+
+// console.log(getPrivateVariable())
+
+var today = new Date();
+
